f70d6db733ef2c62afdb2214d6f8aab370ab86fb
in7264 <in7264@gmail.com>
2025-04-21 21:43:15 +0300

    add task solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,21 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => (
+  <section className="Person">
+    <h2 className="Person__name">My name is {person.name}</h2>
+    {person.age ? <p className="Person__age">I am {person.age}</p> : null}
+    <MaritalInfo person={person} />
+  </section>
+);
+
+function MaritalInfo({ person }) {
+  let message = '';
+
+  if (person.isMarried === false) {
+    message = 'I am not married';
+  } else if (person.sex === 'm') {
+    message = `${person.partnerName} is my wife`;
+  } else {
+    message = `${person.partnerName} is my husband`;
+  }
+
+  return <p className="Person__partner">{message}</p>;
+}
